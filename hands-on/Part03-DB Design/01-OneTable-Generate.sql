11cd0ecb4bfd08757368c91c5de20e0789d6cba8
Dont-wait <nguyentansang@gmail.com>
2024-09-25 21:45:06 +0700

    update Part03

--- hands-on/Part03-DB Design/01-OneTable-Generate.sql
+++ hands-on/Part03-DB Design/01-OneTable-Generate.sql
@@ -1,8 +1,58 @@
-﻿CREATE TABLE Student (
+﻿
+--GENERATE TỪ ERD TRONG TOOL THIẾT KẾ
+CREATE TABLE Student (
   StudentID char(8) NOT NULL, 
   LastName  varchar(50) NOT NULL, 
-  FirstName int NOT NULL, 
-  PRIMARY KEY (StudentID));
-
+  FirstName varchar(50) NOT NULL, 
+ PRIMARY KEY (StudentID)); --tự db engine đặt tên cho rb
+--DROP TABLE Student
+SELECT * FROM Student
+INSERT INTO Student VALUES('SE1234', N'Nguyễn', N'An');
 -- POWER DESIGNER VS. VISUAL PARADIGM
 --			ĐỨC-SAP
+
+
+-----------------------------------------------------------
+-- HỌC THÊM VỀ CÁI CONSTRAINS - TRONG ĐÓ PK CONSTRAINT
+-- Ràng buộc là ác ta/db designer ép cell/cột nào đó value phải ntn
+-- dặt ra quy tắc/rule cho việc nhập data
+-- vì có nhiều quy tắc, nên tránh nhầm lẫn, dễ kiểm soát ta sẽ có quyền
+-- đặt tên cho các quy tắc, contraint name
+-- ví dụ: Má ở nhà đặt quy tắc/nội quy cho mình
+-- Rule#1: vào SG học thật tốt nha con. Tốt: điểm tb >= 8 && ko rớt môn nào
+--				&& học kì ra trường &&  đổi chuyên ngành
+-- Rule#2: Tối đi chơi về nhà sớm.Sớm: trong tối cùng ngày, trước 10h khuya
+-- Rule#3: ???
+-- Tên ràng buộc/quy tắc			nội dung/cái data đc gài vào
+--		Pk__????							PRIMARY KEY
+-- Mặc định các DB Engine nó tự đặt tên cho các RB nó thấy khi bạn gõ câu
+-- lệnh DDL
+-- DB En cho mình cơ chế tự đặt tên RB
+CREATE TABLE StudentV6 (
+  StudentID char(8) NOT NULL, 
+  LastName  varchar(50) NOT NULL, 
+  FirstName varchar(50) NOT NULL, 
+ -- PRIMARY KEY (StudentID)); --tự db engine đặt tên cho rb
+ CONSTRAINT PK_STUDENTSV6 PRIMARY KEY (StudentID)
+);
+
+
+-- DÂN PRO ĐÔI KHI CÒN LÀM CÁCH SAU, NGƯỜI TA TÁCH HẲN VIỆC TẠO RB KHOÁ CHÍNH, KHOÁ NGOẠI
+-- RA HẲN CẤU TRÚC TABLE, TỨC LÀ CREATE TABLE CHỈ CHỨA TÊN CẤU TRÚC - CỘT - DOMAIN
+-- TẠO TABLE XONG RỒI CHỈNH SỬA TABLE - SỬA CÁI TỦ CHỨ KHÔNG PHẢI DATA TRONG TỦ 
+-- ALTER: THAY ĐỔI KẾT CẤU CÁI TỦ, TOÀ NHÀ, CHỨ KHÔNG PHẢI DATA
+DROP TABLE StudentV7
+
+CREATE TABLE StudentV7 (
+  StudentID char(8) NOT NULL, 
+  LastName  varchar(50) NOT NULL, 
+  FirstName varchar(50) NOT NULL, 
+);
+--NẾU TRONG TÌNH HUỐNG CONSTRAINT KEY = NULL NGAY TỪ ĐẦU, THÌ KHÔNG ĐƯỢC PHÉP ALTER TABLE ADD CONTRAINST NX
+
+ -- PRIMARY KEY (StudentID)); --tự db engine đặt tên cho rb
+ --CONSTRAINT 
+ALTER TABLE StudentV7 ADD CONSTRAINT PK_STUDENTSV7 PRIMARY KEY (StudentID)
+
+-- XOÁ 1 RÀNG BUỘC ĐƯỢC KHÔNG, CHO ADD THÌ CHO T DROP
+ALTER TABLE StudentV7 DROP CONSTRAINT PK_STUDENTSV7 
---
+++ hands-on/Part03-DB Design/02-TwoTable-1N.sql
@@ -0,0 +1,65 @@
+﻿CREATE DATABASE DBDESIGN_ONEMANY
+USE DBDESIGN_ONEMANY
+
+--TABLE 1 TẠO TRƯỚC, TABLE N TẠO SAU
+CREATE TABLE MajorV1 (
+	MajorID char(2) PRIMARY KEY, --mặc định db engine tự tạo tên tàng buộc
+	MajorName nvarchar(40) NOT NULL,
+	--....
+)
+--CHÈN DATA   MUA QUẦN ÁO BỎ VÀO TỦ
+INSERT INTO MajorV1 VALUES('SE', N'Kĩ thuật phần mềm')
+INSERT INTO MajorV1 VALUES('IA', N'An toàn thông tin')
+
+DROP TABLE StudentV1
+CREATE TABLE StudentV1 (
+	StudentID char(8) NOT NULL, 
+	LastName  nvarchar(50) NOT NULL, 
+	FirstName nvarchar(50) NOT NULL, 
+	DOB datetime NULL,
+	Address nvarchar(50) null,
+	MID char(2),	--tên cột khoá ngoại/tham chiếu ko cần trùng với bên key
+				-- bắt buộc trùng  100% kiểu dữ liệu, CẦN THAM CHIẾU DATA THÔI
+   --CONSTRAINT PK_STUDENTV1 PRIMARY KEY(StudentID)
+);
+ALTER TABLE StudentV1 ADD CONSTRAINT PK_STUDENTV1 PRIMARY KEY(StudentID)
+--CHÈN DATA SINH VIEN
+SELECT * FROM StudentV1
+SELECT * FROM MajorV1
+INSERT INTO StudentV1 VALUES('SE1', N'Nguyễn', N'AN', NULL, NULL, 'SE')
+
+--THIẾT KẾ TRÊN SAI VÌ ... KO CÓ THAM CHIẾU GIỮA MAJORID CỦA STUDENT  VS MAJOR PHÍA TRÊN
+INSERT INTO StudentV1 VALUES('SE2', N'Nguyễn', N'bíNH', NULL, NULL, 'HI')
+
+
+
+
+CREATE TABLE MajorV2 (
+	MajorID char(2) PRIMARY KEY, --mặc định db engine tự tạo tên tàng buộc
+	MajorName nvarchar(40) NOT NULL,
+	--....
+)
+--CHÈN DATA   MUA QUẦN ÁO BỎ VÀO TỦ
+INSERT INTO MajorV2 VALUES('SE', N'Kĩ thuật phần mềm')
+INSERT INTO MajorV2 VALUES('IA', N'An toàn thông tin')
+DROP TABLE MajorV2
+DROP TABLE StudentV2
+CREATE TABLE StudentV2 (
+	StudentID char(8) PRIMARY KEY, 
+	LastName  nvarchar(50) NOT NULL, 
+	FirstName nvarchar(50) NOT NULL, 
+	DOB datetime NULL,
+	Address nvarchar(50) null,
+	--MID char(2) REFERENCES MajorV2(MajorID)	
+	MajorID char(2) FOREIGN KEY REFERENCES MajorV2(MajorID)
+	--tớ chọn chuyên ngành ở trên kia kìa, xin tham chiếu trên kia
+);
+SELECT * FROM MajorV2
+INSERT INTO StudentV2 VALUES('SE1', N'Nguyễn', N'AN', NULL, NULL, 'SE')
+
+INSERT INTO StudentV1 VALUES('SE2', N'Nguyễn', N'bíNH', NULL, NULL, 'HI')
+
+SELECT * FROM StudentV2
+
+-- KHÔNG ĐƯỢC XOÁ TABLE 1 NẾU NÓ ĐANG ĐC THAM CHIẾU BỞI THẰNG KHÁC
+-- NẾU CÓ MỐI QUAN HỆ 1-N, XOÁ N TRƯỚC RỒI MỚI XOÁ 1 SAU
